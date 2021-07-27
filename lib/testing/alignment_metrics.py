04c89b30fcab42cdf6a618ae09ebf357f462dc64
emreerhan <emre.erhan@10xgenomics.com>
2021-07-27 12:27:51 -0700

    sam and paf comparison

--- lib/testing/alignment_metrics.py
+++ lib/testing/alignment_metrics.py
@@ -58,22 +58,26 @@ def main():
 
     for row1, row2 in zip(reader1, reader2):
         n_reads += 1
-        if reader1_type == "paf":
+        if reader1_type == "paf" and reader2_type == "paf":
             row1 = parse_paf_alignment(row1[:-1])
             n_in1_identical_align += paf_query_identical_to_reference(row1)
-            print(row1)
-        else:
-            n_in1_identical_align += sam_query_identical_to_reference(row1)
-            row1 = convert_sam_to_paf_alignment(row1)
-        if reader2_type == "paf":
             row2 = parse_paf_alignment(row2[:-1])
             n_in2_identical_align += paf_query_identical_to_reference(row2)
-        else:
+            n_concordant_align += row1 == row2
+        elif reader1_type == "sam" and reader2_type == "sam":
+            n_in1_identical_align += sam_query_identical_to_reference(row1)
             n_in2_identical_align += sam_query_identical_to_reference(row2)
-            row2 = convert_sam_to_paf_alignment(row2)
-            print(row2)
-
-        n_concordant_align += row1 == row2
+            n_concordant_align += row1 == row2  # will this work?
+        elif reader1_type == "sam" and reader2_type == "paf":
+            n_in1_identical_align += sam_query_identical_to_reference(row1)
+            row2 = parse_paf_alignment(row2[:-1])
+            n_in2_identical_align += paf_query_identical_to_reference(row2)
+            n_concordant_align += compare_sam_to_paf(row1, row2)
+        elif reader1_type == "paf" and reader2_type == "sam":
+            row1 = parse_paf_alignment(row1[:-1])
+            n_in1_identical_align += paf_query_identical_to_reference(row1)
+            n_in2_identical_align += sam_query_identical_to_reference(row2)
+            n_concordant_align += compare_sam_to_paf(row2, row1)
 
     print(f"paf1 identical alignment to ref fraction: {n_in1_identical_align/n_reads}")
     print(f"paf2 identical alignment to ref fraction: {n_in2_identical_align/n_reads}")
@@ -84,7 +88,7 @@ def get_alignment_reader(path: str) -> Iterable:
     _, ext = os.path.splitext(path)
     if ext == ".bam" or ext == ".sam":
         samfile = pysam.AlignmentFile(path)
-        return (samfile.fetch(), "sam")  # will this work?
+        return (samfile.fetch(), "sam")
     elif ext == ".paf":
         return (csv.reader(open(path), delimiter="\t"), "paf")
 
@@ -115,17 +119,64 @@ def convert_sam_to_paf_alignment(alignment: pysam.AlignedSegment) -> namedtuple:
         alignment.query_name,
         str(alignment.infer_query_length()),
         str(alignment.query_alignment_start),
-        str(alignment.query_alignment_start + alignment.query_length),
+        str(alignment.query_alignment_start + alignment.infer_query_length() - 1),
         strand,
         alignment.reference_name,
-        str(alignment.reference_end),
+        alignment.reference_length,
         str(alignment.reference_start),
         str(alignment.reference_end - 1),
-        len(alignment.get_aligned_pairs()),
+        str(len(alignment.get_aligned_pairs())),
         str(alignment.query_alignment_length),
         str(alignment.mapping_quality),
     )
 
 
+def compare_sam_to_paf(
+    sam_alignment: pysam.AlignedSegment, paf_alignment: namedtuple
+) -> int:
+    strand = "-" if sam_alignment.is_reverse else "+"
+    comparison = (
+        (sam_alignment.query_name == paf_alignment.query_name)
+        and (str(sam_alignment.infer_query_length()) == paf_alignment.query_len)
+        and (str(sam_alignment.query_alignment_start) == paf_alignment.query_start)
+        and (
+            str(
+                sam_alignment.query_alignment_start
+                + sam_alignment.infer_query_length()
+                - 1
+            )
+            == paf_alignment.query_end
+        )
+        and (strand == paf_alignment.strand)
+        and (sam_alignment.reference_name == paf_alignment.target_name)
+        and (str(sam_alignment.reference_start) == paf_alignment.target_start)
+        and (str(sam_alignment.reference_end - 1) == paf_alignment.target_end)
+        and (
+            str(len(sam_alignment.get_aligned_pairs()))
+            == paf_alignment.num_matches_residues
+        )
+        and (str(sam_alignment.mapping_quality) == paf_alignment.mapping_qual)
+    )
+    return comparison
+
+
 if __name__ == "__main__":
     main()
+
+PAF_TUPLE = namedtuple(
+    "paf_record",
+    [
+        "query_name",
+        "query_len",
+        "query_start",
+        "query_end",
+        "strand",
+        "target_name",
+        "target_len",
+        "target_start",
+        "target_end",
+        "num_match_residues",
+        "alignment_len",
+        "mapping_qual",
+    ],
+)
